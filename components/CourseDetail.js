b89fd83465d493a37bcadaa16abe10015388fb41
Clark Lu <clarklubeijing@gmail.com>
2021-11-09 15:48:27 -0600

    pass params

--- components/CourseDetail.js
+++ components/CourseDetail.js
@@ -3,11 +3,12 @@ import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
 
 const CourseDetail = (course) => {
 
-    alert(course.route.params.subject + course.route.params.subject.number)
+    let subject = course.route.params.subject
+    let number = course.route.params.number
 
     return (
     <Text>
-        Hi
+        {subject} {number}
     </Text>
     )
 }
