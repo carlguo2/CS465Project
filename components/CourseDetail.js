efe39fd9953c98ed9b072365d6f65a8a0844531f
Carl Guo <carlguo2@illinois.edu>
2021-11-10 18:39:11 -0600

    get course data to display on page

--- components/CourseDetail.js
+++ components/CourseDetail.js
@@ -1,15 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Pressable, ScrollView } from 'react-native';
+import courseData from "../backend/courses_new.json";
 
 const CourseDetail = (course) => {
 
-    let subject = course.route.params.subject
-    let number = course.route.params.number
+    let {subject, number} = course.route.params;
+
+    let classList = ['400', '401', '407'];
+    
+    function retrieveCourseData() {
+        var courses = [];
+        for (var i = 0; i < courseData.length; i++) {
+            if (subject === courseData[i].Subject && (number === "" || parseInt(number) === courseData[i].Number)) {
+                courses.push(courseData[i]);
+            }
+        }
+        return courses.map((course) => {
+            return (
+                <View>
+                    <Text>
+                    {course.Subject + ":" + course.Number}
+                    </Text>
+                </View>
+            )
+        })
+    }
 
     return (
-    <Text>
-        {subject} {number}
-    </Text>
+    // <Text>
+    //     {subject} {number}
+    // </Text>
+    <ScrollView>
+        {/* <Text>
+            Search result for
+        </Text>
+        <Text>
+            {subject}
+        </Text>
+        <Text>
+            {number}
+        </Text> */}
+        { retrieveCourseData() }
+    </ScrollView>
     )
 }
 
