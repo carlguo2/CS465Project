5a5bc6235c07c1725ff80c2e3bffc97584c35336
Carl Guo <carlguo2@illinois.edu>
2021-11-11 15:44:53 -0600

    separate modal into new component

--- components/CourseDetail.js
+++ components/CourseDetail.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, Modal, View, TextInput, Pressable, ScrollView } from 'react-native';
 import { fonts } from "react-native-elements/dist/config";
-import courseData from "../backend/courses_new.json";
+import courseData from "../backend/courses.json";
+import CourseModal from "./CourseModal"
 
 const CourseDetail = (course) => {
 
     let {subject, number} = course.route.params;
-
-    let classList = ['400', '401', '407'];
     
     function retrieveCourseData() {
         var courses = [];
@@ -20,43 +19,11 @@ const CourseDetail = (course) => {
             const [modalVisible, setModalVisible] = useState(false);
             return (
                 <View style={styles.centeredView}>
-                    <Modal
-                        animationType="slide"
-                        transparent={true}
-                        visible={modalVisible}
-                        onRequestClose={() => {
-                        Alert.alert("Modal has been closed.");
-                        setModalVisible(!modalVisible);
-                        }}
-                    >
-                        <View style={styles.centeredView}>
-                        <View style={styles.modalView}>
-                            <Text style={styles.modalTextTitle}>{course.Subject + "   " + course.Number + "   "}</Text>
-                            <Text style={styles.modalTextName}>{course.Name}</Text>
-                            <Text style={styles.modalText}>{"Hours: " + course["Credit Hours"][0]}</Text>
-                            <Text style={styles.modalText}>{"Section: " + course.Section}</Text>
-                            <Text style={styles.modalText}>{"Type: " + course["Type Code"]}</Text>
-                            <Text style={styles.modalText}>{"Instructor: " + course.Instructors}</Text>
-                            <Text style={styles.modalText}>{"Location: " + course.Building + " " + course.Room}</Text>
-                            <Text style={styles.modalTextPrereq}>{course["Section Info"]}</Text>
-                            <Text style={styles.modalTextName}>Times</Text>
-                            <View style={[styles.container, {flexDirection: "row"}]}>
-                            <Pressable
-                            style={[styles.button, styles.buttonAdd]}
-                            onPress={() => alert("Added!")}
-                            >
-                            <Text style={styles.textStyle}>Add</Text>
-                            </Pressable>
-                            <Pressable
-                            style={[styles.button, styles.buttonClose]}
-                            onPress={() => setModalVisible(!modalVisible)}
-                            >
-                            <Text style={styles.textStyle}>Cancel</Text>
-                            </Pressable>
-                            </View>
-                        </View>
-                        </View>
-                    </Modal>
+                    <CourseModal 
+                        modalVisible={modalVisible}
+                        setModalVisible={setModalVisible}
+                        course={course}
+                    />
                     <Pressable key="{course.CRN}"  style = {styles.entry}
                     onPress={() => {
                         setModalVisible(true)
@@ -77,33 +44,13 @@ const CourseDetail = (course) => {
     )
 }
 
-export default CourseDetail
-
 const styles = StyleSheet.create({
     centeredView: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
         marginTop: 22,
-      },
-      modalView: {
-        width: "80%",
-        margin: 20,
-        backgroundColor: "white",
-        borderRadius: 20,
-        padding: 35,
-        alignItems: "center",
-        shadowColor: "#000",
-        shadowOffset: {
-          width: 0,
-          height: 2
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-        borderColor: "#ff5e3b",
-        borderWidth: 5,
-      },
+    },
     text: {
         fontSize: 30,
     },
@@ -118,47 +65,7 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         height: 90,
         alignItems: "center"
-    },
-    button: {
-        borderRadius: 20,
-        padding: 10,
-        elevation: 2
-      },
-      buttonOpen: {
-        backgroundColor: "#F194FF",
-      },
-      buttonAdd: {
-          backgroundColor: "#2196F3",
-          marginRight: 40
-      },
-      buttonClose: {
-        backgroundColor: "#c4c4c4",
-      },
-      textStyle: {
-        color: "white",
-        fontWeight: "bold",
-        textAlign: "center"
-      },
-      modalText: {
-        marginBottom: 15,
-        textAlign: "center",
-        fontSize: 16
-      },
-      modalTextPrereq: {
-        marginBottom: 15,
-        textAlign: "center",
-        fontSize: 16,
-        backgroundColor: "#fcf9b8",
-        padding: 5
-      },
-      modalTextTitle: {
-        marginBottom: 15,
-        textAlign: "center",
-        fontSize: 24
-      },
-      modalTextName: {
-        marginBottom: 15,
-        textAlign: "center",
-        fontSize: 19
-      }
+    }
 });
+
+export default CourseDetail
